a2c6a99c8c9b6d1aaf994dde710a95d6527e822c
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-07-26 08:27:34 +0100

    Update the author info in the home page

--- views/index.view.php
+++ views/index.view.php
@@ -9,14 +9,15 @@
 
     </div>
     <div class="author__info">
-      <h1 class="author__name">Fisayo Michael Animashaun</h1>
+      <h1 class="author__name">Animashaun F.M</h1>
       <p class="author__bio">I am a registered Mechanical Engineer and an experiened facilities manager transitioning to
-        full-stack developmen, proficient in React, JavaScript, PHP, Moodle LMS, cloud hosting, WHM, cPanel, databases,
-        and web services management. Crafting tailored features for optimal performance.</p>
+        full-stack development, proficient in React, JavaScript, PHP, Moodle LMS, cloud hosting, WHM, cPanel, databases,
+        and web services management.</p>
       <div class="autor__social">
-        <a href="" class="author__social--link"><i class="fab fa-facebook-square"></i></a>
-        <a href="" class="author__social--link"><i class="fab fa-twitter-square"></i></a>
-        <a href="" class="author__social--link"><i class="fab fa-instagram"></i></a>
+        <a href="https://facebook.com" class="author__social--link"><img src="img/facebook.svg" alt="facebook icon"></a>
+        <a href="https://twitter.com" class="author__social--link"><img src="img/twitter.svg" alt="twitter icon"></a>
+        <a href="https://behance.com" class="author__social--link"><img src="img/behance.svg" alt="behance icon"></a>
+        <a href="https://linkedin.com" class="author__social--link"><img src="img/linkedin.svg" alt="linkedin icon"></a>
       </div>
     </div>
   </div>
